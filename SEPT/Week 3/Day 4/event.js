b01b040c119230b45090d7225937fa654df576b0
chandankumardey053 <deychandankumar584@gmail.com>
2025-09-21 21:34:47 +0530

    EVENT

--- SEPT/Week 3/Day 4/event.js
+++ SEPT/Week 3/Day 4/event.js
@@ -1,26 +1,46 @@
-console.log("Today we are going to learn event");
+console.log("Today we are going to learn Event")
 
-const  button=document.getElementById("btn");
-button.onclick=()=>{
-      document.body.style.backgroundColor="red";
-}
+// const button=document.getElementById("btn");
+// button.onclick=()=>{
+//     document.body.style.backgroundColor="red";
+// }
 
-button.onkeyup=()=>{
-    console.log("Button clicked by keyboard");
-}
+// button.onkeyup=()=>{
+//     console.log("button clicked by keyboard")
+// }
 
-const input=document.querySelector("input");
+// const input=document.querySelector("input");
 
-input.onchange=(e)=>{
-    console.log("input field change",e.target.value);
-}
+// input.onchange=(e)=>{
+//     console.log("input field change",e.target.value)
+// }
 
-const p=document.querySelector("p");
-p.onmousemove=()=>{
-    console.log("mouse over");
-}
+// const p =document.querySelector("p");
+// p.onmousemove=()=>{
+//     console.log("mouse over")
+// }
 
-document.querySelector("form").onsubmit=(e)=>{
-    e.preventDefault();
-    console.log("Form submitted");
-}
+// document.querySelector("form").onsubmit=(e)=>{
+//     e.preventDefault();
+//     console.log("form submitted")
+// }
+
+const form=document.querySelector("form");
+
+const inputs=document.getElementsByTagName("input");
+
+form.addEventListener("submit",(eve)=>{
+    eve.preventDefault();
+    let name=inputs[0].value;
+    let age=inputs[1].value;
+    localStorage.setItem("user",JSON.stringify({name,age}))
+})
+
+const getBtn=document.querySelector("#getItembtn");
+
+getBtn.addEventListener("click",()=>{
+    const Strdata=localStorage.getItem("use");
+    const data=JSON.parse(Strdata);
+    console.log(data)
+    alert("login success")
+})
